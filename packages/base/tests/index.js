32a38d85a8711309a39fe80ba9777a6c3e41013e
Kirill Yakovenko <kirill.yakovenko@me.com>
2017-02-28 21:11:48 +0200

    rename module to base

--- packages/base/tests/index.js
+++ packages/base/tests/index.js
@@ -1,15 +1,10 @@
 import css from '../src';
 
-const props = {
-  active: true
-}
-
 const pos = 'absolute';
 
 const testSheet = css`
   .test-node {
     display: flex;
-    content: ${data => data ? data.foo: 'default'};
     background-position: ${pos};
     &:hover {
       text-decoration: underline;
