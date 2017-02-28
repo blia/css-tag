3b5a9aa55be7a45cad1bbcffadfff1bcb005d818
Kirill Yakovenko <kirill.yakovenko@me.com>
2017-02-28 23:04:32 +0200

    build src

---
+++ packages/base/lib/index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.css = css;
+
+var _transformer = require('./transformer');
+
+var _transformer2 = _interopRequireDefault(_transformer);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var transformer = new _transformer2.default();
+
+function css() {
+  return transformer.transform.apply(transformer, arguments);
+}
+
+exports.default = css;
